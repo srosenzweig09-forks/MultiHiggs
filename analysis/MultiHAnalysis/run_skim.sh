c2e422ae88fe4b9f891976431c1f09ec11e24ccf
Suzanne <srosenzweig09@gmail.com>
2023-02-21 11:19:21 +0100

    Missing exe line

--- analysis/MultiHAnalysis/run_skim.sh
+++ analysis/MultiHAnalysis/run_skim.sh
@@ -3,6 +3,8 @@
 # eval `scramv1 runtime -sh`
 # source scripts/setup.sh
 
+exe=bin/skim_ntuple.exe
+
 # output="NMSSM_XYH_YToHH_6b_MX_700_MY_400_accstudies_500k_Jul2021-v2.root"
 # output="output-tree.root"
 output="output.root"
@@ -11,10 +13,7 @@ output="output.root"
 # input="input/Run2_UL/2018/QCD_bEnriched_HT500to700_TuneCP5_13TeV-madgraph-pythia8.txt"
 input="input/PrivateMC_2018/NMSSM_XYH_YToHH_6b/NMSSM_XYH_YToHH_6b_MX_700_MY_400.txt --is-signal"
 
-cfg="config/skim_ntuple_2018_presel.cfg"
-# cfg="config/skim_ntuple_2018_8b.cfg"
-# cfg="config/skim_ntuple_2018_qcd.cfg"
-# cfg="config/skim_ntuple_2018_cr.cfg"
+cfg="config/skim_ntuple_2018_106X_NanoAODv9.cfg"
      
 make exe -j && \
     $exe \
