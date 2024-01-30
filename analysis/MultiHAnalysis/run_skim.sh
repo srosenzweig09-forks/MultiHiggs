52ebbe1d9e0e4d972ed9714f73d48217662bda77
Suzanne <srosenzweig09@gmail.com>
2024-01-30 13:46:03 +0100

    Testing 2017

--- analysis/MultiHAnalysis/run_skim.sh
+++ analysis/MultiHAnalysis/run_skim.sh
@@ -9,11 +9,26 @@ exe=bin/skim_ntuple.exe
 # output="output-tree.root"
 output="output.root"
 
+extra=""
+
 # input="input/Run2_UL/2018/JetHT_Run2018C.txt --is-data "
+# input="input/Run2_UL/RunIISummer20UL17NanoAODv9/BTagCSV_Run2017B.txt --is-data "
+# input="input/Run2_UL/RunIISummer20UL16NanoAODv9/BTagCSV_Run2016B_ver2_HIPM.txt --is-data "
+
 # input="input/Run2_UL/2018/QCD_bEnriched_HT500to700_TuneCP5_13TeV-madgraph-pythia8.txt"
-input="input/PrivateMC_2018/NMSSM_XYH_YToHH_6b/NMSSM_XYH_YToHH_6b_MX_700_MY_400.txt --is-signal"
+# input="input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_bEnriched_HT700to1000_TuneCP5_13TeV-madgraph-pythia8.txt"
+# input="input/Run2_UL/RunIISummer20UL18NanoAODv9/small_TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8.txt"
+
+# input="input/Run2_UL/RunIISummer20UL16NanoAODv9/NMSSM_XToYHTo6B/NMSSM_XToYHTo6B_MX-1000_MY-600_TuneCP5_13TeV-madgraph-pythia8.txt --is-signal"
+# input="input/Run2_UL/RunIISummer20UL16NanoAODv9/preVFP/NMSSM_XToYHTo6B/NMSSM_XToYHTo6B_MX-1000_MY-250_TuneCP5_13TeV-madgraph-pythia8.txt --is-signal"
+# input="input/Run2_UL/RunIISummer20UL17NanoAODv9/NMSSM_XToYHTo6B/NMSSM_XToYHTo6B_MX-700_MY-400_TuneCP5_13TeV-madgraph-pythia8.txt --is-signal"
+# input="input/Run2_UL/RunIISummer20UL18NanoAODv9/NMSSM_XToYHTo6B/NMSSM_XToYHTo6B_MX-1000_MY-250_TuneCP5_13TeV-madgraph-pythia8.txt --is-signal"
+input="input/PrivateMC_2018/NMSSM_XYH_YToHH_6b/NMSSM_XYH_YToHH_6b_MX_700_MY_400_2M.txt --is-signal"
 
 cfg="config/skim_ntuple_2018_106X_NanoAODv9.cfg"
+# cfg="config/skim_ntuple_2017_106X_NanoAODv9.cfg"
+# cfg="config/skim_ntuple_2016postVFP_106X_NanoAODv9.cfg"
+# cfg="config/skim_ntuple_2016preVFP_106X_NanoAODv9.cfg"
      
 make exe -j && \
     $exe \
@@ -21,4 +36,5 @@ make exe -j && \
 	--cfg  $cfg \
 	--output $output \
 	--maxEvts 100 \
+	$extra \
 	$@
