0e684b56df142fcb7bcb53e4c732aa67e2013d59
Suzanne <srosenzweig09@gmail.com>
2024-02-01 13:04:52 +0100

    Introduced 6b_scripts

--- analysis/MultiHAnalysis/scripts/condortools/scriptgen.py
+++ analysis/MultiHAnalysis/scripts/condortools/scriptgen.py
@@ -38,6 +38,7 @@ def make_exec_script(filename, analysis_tarball, filelist_proto, outfile_proto, 
     fout = open(filename, 'w')
     #------------------ headers
     writeln(fout, '#!/bin/bash')
+    writeln(fout, 'set -e') ## exit on error
     writeln(fout, '{') ## start of redirection..., keep stderr and stdout in a single file, it's easier
     # writeln(fout, 'set -x') ## alternatively, can have every typed command redirected to stderr with this
     writeln(fout, 'IJOB=$1')  #job number    
@@ -60,6 +61,12 @@ def make_exec_script(filename, analysis_tarball, filelist_proto, outfile_proto, 
     writeln(fout, 'rm %s' % tarname)
     writeln(fout, 'echo "... retrieving filelist"')
     writeln(fout, 'xrdcp -f -s %s .' % flist_name) ## force overwrite file list
+    writeln(fout, 'mkdir files')
+    writeln(fout, 'for file in `cat $(basename %s)`; do' % flist_name)
+    writeln(fout, '   echo "xrdcp -f -s ${file} files/"')
+    writeln(fout, '   xrdcp -f -s ${file} files/')
+    writeln(fout, 'done')
+    writeln(fout, 'ls files/* > $(basename %s)' % flist_name)
     #------------------ environment configurations
     writeln(fout, 'export CPP_BOOST_PATH=%s' % cpp_boost_path)
     writeln(fout, 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./lib:${CPP_BOOST_PATH}/lib')
